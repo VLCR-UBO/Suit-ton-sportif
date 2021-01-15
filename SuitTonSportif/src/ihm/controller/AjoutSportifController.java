bfe5ee3b589b6b75cd04d53f27de20f55a0f57ab
Chiara <chiara.relevat@outlook.fr>
2021-01-15 15:18:21 +0100

    interface sportif fonctionelle

--- SuitTonSportif/src/ihm/controller/AjoutSportifController.java
+++ SuitTonSportif/src/ihm/controller/AjoutSportifController.java
@@ -2,32 +2,59 @@ package ihm.controller;
 
 import java.io.IOException;
 import java.net.URL;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Date;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.Node;
+import javafx.stage.Stage;
 
-public class AjoutSportifController implements Initializable{
-	@FXML private TextField nom;
-	@FXML private TextField prenom;
-	@FXML private TextField pseudo;
-	@FXML private DatePicker dateNaissance;
-	
-	@Override
-	public void initialize(URL arg0, ResourceBundle arg1) {
-		// TODO Auto-generated method stub
-		
-	}
-	
-	@FXML
-	public void fermerPopUp(MouseEvent mouseEvent) throws IOException {
-		
-	}
-	
-	@FXML
-	public void creationSportif(MouseEvent mouseEvent) throws IOException {
-		
-	}
+public class AjoutSportifController implements Initializable {
+  @FXML
+  private TextField nom;
+  @FXML
+  private TextField prenom;
+  @FXML
+  private TextField pseudo;
+  @FXML
+  private DatePicker dateNaissance;
+
+  @Override
+  public void initialize(URL arg0, ResourceBundle arg1) {
+    nom.setText("nom");
+    prenom.setText("prenom");
+    pseudo.setText("pseudo");
+    Date date = new Date(System.currentTimeMillis());
+    dateNaissance.setValue(
+        Instant.ofEpochMilli(date.getTime()).atZone(ZoneId.systemDefault()).toLocalDate());
+  }
+
+  /**
+   * Ferme la PopUp sans éffectuer d'action.
+   * @param mouseEvent : clique de l'utilisateur
+   * @throws IOException : en cas d'échec de l'ecture du fxml
+   */
+  @FXML
+  public void fermerPopUp(MouseEvent mouseEvent) throws IOException {
+    Stage stage = (Stage) ((Node) mouseEvent.getSource()).getScene().getWindow();
+    stage.close();
+  }
+
+  /**
+   * Creer un sportif grâce aux informations saisie par l'utilisateur, puis ferme la PopUp.
+   * @param mouseEvent : clique de l'utilisateur
+   * @throws IOException : en cas d'échec de l'ecture du fxml
+   */
+  @FXML
+  public void creationSportif(MouseEvent mouseEvent) throws IOException {
+    LocalDate naissance = dateNaissance.getValue();
+    Date date = Date.from(naissance.atStartOfDay(ZoneId.systemDefault()).toInstant());
+    this.fermerPopUp(mouseEvent);
+  }
 }
--- SuitTonSportif/src/ihm/controller/SportifController.java
+++ SuitTonSportif/src/ihm/controller/SportifController.java
@@ -7,7 +7,6 @@ import java.util.List;
 import java.util.ResourceBundle;
 import javafx.event.EventHandler;
 import javafx.collections.FXCollections;
-import javafx.scene.Node;
 import javafx.scene.image.ImageView;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -23,7 +22,6 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import ihm.PopUp;
-import javafx.stage.Stage;
 
 public class SportifController implements Initializable {
   @FXML
@@ -41,9 +39,11 @@ public class SportifController implements Initializable {
   @FXML
   private VBox information;
 
-  private List<String> sportif = new ArrayList<String>();// TO DO : remplacer par les sportif qd ce
-                                                         // sera créer
+  private List<String> sportif = new ArrayList<String>(); // TO DO : remplacer par les sportif qd ce
+                                                          // sera créer
   private List<HBox> lignes;
+  
+  public static String nomSelectionner = null;
 
   @Override
   public void initialize(URL arg0, ResourceBundle arg1) {
@@ -67,7 +67,12 @@ public class SportifController implements Initializable {
       modifier.setOnAction(new EventHandler<ActionEvent>() {
         @Override
         public void handle(ActionEvent e) {
-          modifier(nom);
+          try {
+            modifier(nom);
+          } catch (IOException e1) {
+            // TODO Auto-generated catch block
+            e1.printStackTrace();
+          }
         }
       });
 
@@ -94,6 +99,11 @@ public class SportifController implements Initializable {
     list.setItems(items);
   }
 
+  /**
+   * Lance la PopUp pour ajouter un sportif à la liste.
+   * @param mouseEvent : clique de l'utilisateur
+   * @throws IOException : en cas d'échec de l'ecture du fxml
+   */
   @FXML
   public void ajoutSportif(MouseEvent mouseEvent) throws IOException {
     final URL fxmlUrl = getClass().getResource("../view/ajoutSportif.fxml");
@@ -104,6 +114,15 @@ public class SportifController implements Initializable {
     popup.display();
   }
   
+  /**
+   * Afficher dans la partie de droite les informations du sportif : 
+   *  - nom
+   *  - prénom
+   *  - pseudo 
+   *  - date de naissance
+   *  - sports.
+   * @param mouseEvent : clique de l'utilisateur
+   */
   @FXML
   public void afficherInformationSportif(MouseEvent mouseEvent)  {
     information.setVisible(true);
@@ -112,10 +131,26 @@ public class SportifController implements Initializable {
     pseudo.setText(sportif.getText());
   }
 
-  public void modifier(String nom) {
+  /**
+   * Ouvre la PopUp pour modifier les infos du sportif.
+   * @param nom : nom du sportif à modifier
+   * @throws IOException : en cas d'échec de l'ecture du fxml
+   */
+  public void modifier(String nom) throws IOException {
+    nomSelectionner = nom;
+    final URL fxmlUrl = getClass().getResource("../view/ajoutSportif.fxml");
+    final FXMLLoader fxmlLoader = new FXMLLoader(fxmlUrl);
+    Pane root = fxmlLoader.load();
 
+    PopUp popup = new PopUp(root, "Modification du sportif");
+    popup.display();
   }
 
+  /**
+   * Supprime un sportif de la liste.
+   * @param nom : nom du sportif à supprimer
+   * @param hb : HBox à supprimer de la liste view
+   */
   public void supprimer(String nom, HBox hb) {
     sportif.remove(nom);
     list.getItems().remove(hb);
