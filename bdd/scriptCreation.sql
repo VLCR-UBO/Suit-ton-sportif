c72da913c33403fd1963cf8ad4087ff7e04df4ba
Yann <ychantrel@gmail.com>
2021-01-25 17:19:24 +0100

    creation de la bdd

--- bdd/scriptCreation.sql
+++ bdd/scriptCreation.sql
@@ -67,3 +67,72 @@ CREATE TABLE IF NOT EXISTS `enregistretonsportif`.`QUESTIONNAIRE` (
   PRIMARY KEY (`intituleQuestionnaire`))
 ENGINE = InnoDB;
 
+-- -----------------------------------------------------
+-- Table `enregistretonsportif`.`RELATION_SPORTIF_QUESTIONNAIRE`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `enregistretonsportif`.`RELATION_SPORTIF_QUESTIONNAIRE` ;
+
+CREATE TABLE IF NOT EXISTS `enregistretonsportif`.`RELATION_SPORTIF_QUESTIONNAIRE` (
+  `idRelationSportifQuestionnaire` INT NOT NULL AUTO_INCREMENT,
+  `unSportif` INT NOT NULL,
+  `unQuestionnaire` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`idRelationSportifQuestionnaire`),
+  INDEX `fk_RELATION_SPORTIF_QUESTIONNAIRE_SPORTIF_idx` (`unSportif` ASC),
+  INDEX `fk_RELATION_SPORTIF_QUESTIONNAIRE_QUESTIONNAIRE_idx` (`unQuestionnaire` ASC),
+  CONSTRAINT `fk_RELATION_SPORTIF_QUESTIONNAIRE_SPORTIF`
+    FOREIGN KEY (`unSportif`)
+    REFERENCES `enregistretonsportif`.`SPORTIF` (`idSportif`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_RELATION_SPORTIF_QUESTIONNAIRE_QUESTIONNAIRE`
+    FOREIGN KEY (`unQuestionnaire`)
+    REFERENCES `enregistretonsportif`.`QUESTIONNAIRE` (`intituleQuestionnaire`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+-- -----------------------------------------------------
+-- Table `enregistretonsportif`.`REPONSE`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `enregistretonsportif`.`REPONSE` ;
+
+CREATE TABLE IF NOT EXISTS `enregistretonsportif`.`REPONSE` (
+  `idReponse` INT NOT NULL,
+  `numeroSemaine` INT NOT NULL,
+  `derniereModification` DATE NOT NULL,
+  `valeurReponse` INT NOT NULL,
+  `unSportif` INT NOT NULL,
+  PRIMARY KEY (`idReponse`),
+  INDEX `fk_REPONSE_SPORTIF_idx` (`unSportif` ASC),
+  CONSTRAINT `fk_REPONSE_SPORTIF`
+    FOREIGN KEY (`unSportif`)
+    REFERENCES `enregistretonsportif`.`SPORTIF` (`idSportif`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `enregistretonsportif`.`QUESTION`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `enregistretonsportif`.`QUESTION` ;
+
+CREATE TABLE IF NOT EXISTS `enregistretonsportif`.`QUESTION` (
+  `intituleQuestion` VARCHAR(45) NOT NULL,
+  `reponseParDefaut` INT NOT NULL,
+  `unQuestionnaire` VARCHAR(45) NOT NULL,
+  `uneReponse` INT NOT NULL,
+  PRIMARY KEY (`intituleQuestion`),
+  INDEX `fk_QUESTION_QUESTIONNAIRE_idx` (`unQuestionnaire` ASC),
+  INDEX `fk_QUESTION_REPONSE_idx` (`uneReponse` ASC),
+  CONSTRAINT `fk_QUESTION_QUESTIONNAIRE`
+    FOREIGN KEY (`unQuestionnaire`)
+    REFERENCES `enregistretonsportif`.`QUESTIONNAIRE` (`intituleQuestionnaire`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_QUESTION_REPONSE`
+    FOREIGN KEY (`uneReponse`)
+    REFERENCES `enregistretonsportif`.`REPONSE` (`idReponse`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
