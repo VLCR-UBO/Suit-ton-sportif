a07f601dcc7f2746884a064057a406acd8fec846
aVadeleau <alanvadeleau@live.fr>
2021-02-02 16:54:21 +0100

    Ajout DAO serveur

---
+++ web/serveur/models/QuestionDAO.js
@@ -0,0 +1,10 @@
+const db  = require('./connexionBDD.js');
+const QuestionnDAO = function(){
+
+    this.findByKey = function(key, callback){
+        db.query("SELECT * FROM QUESTION WHERE unQuestionnaire = ?", [key], callback);
+    }
+};
+
+const dao = new QuestionnaireDAO();
+module.exports = dao;
---
+++ web/serveur/models/ReponseDAO.js
@@ -0,0 +1,11 @@
+const db = require('./connexionBDD.js');
+const ReponseDAO = function(){
+
+    this.insert = function(values, callback){
+        var request = "INSERT INTO REPONSE VALUES(?, ?, ?, ?, ?)";
+        const stmt = [values.idReponse, values.numeroSemaine, values.derniereModification, values.valeurReponse, values.unSportif ];
+    }    
+};
+
+const dao = new QuestionnaireDAO();
+module.exports = dao;
