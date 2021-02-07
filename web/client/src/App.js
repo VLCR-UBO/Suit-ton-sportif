67d05ef6c57865cb1f065bcd9b48c165341d4ce8
Chiara <chiara.relevat@outlook.fr>
2021-02-07 13:00:42 +0100

    liste questionnaire

--- web/client/src/App.js
+++ web/client/src/App.js
@@ -1,15 +1,37 @@
 import Header from './components/layout/Header';
 import Questionnaire from './components/Questionnaire';
+import React, {Component} from 'react';
 import Login from './components/Login';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Login />
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      charger : 'connexion'
+    }
+  }
+
+  changerPage = (valeur) => {
+    this.setState({
+      charger : valeur
+    });
+  }
+
+  render(){
+    if(this.state.charger === 'connexion'){
+      return (
+        <div className="App">
+          <Header />
+          <Login />
+        </div>
+      );
+    }else if(this.state.charger === 'questionnaire'){
+      return(
+        <Questionnaire/>
+      );
+    }
+  }
 }
 
 export default App;
