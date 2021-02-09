12458b7f4620436902a6f208d3edb8ffe3626399
Leborgne Alex <leborgne.alex1@gmail.com>
2021-02-09 15:56:26 +0100

    modif question

--- web/client/src/components/Question.js
+++ web/client/src/components/Question.js
@@ -8,20 +8,18 @@ export default function Question() {
             <div class="col-4">
                 <div class="card" id="card">
                     <div class="container">
-                        <div class="progress">
-                            <div class="progress-bar" style={{width:"70%"}}>70%</div>
-                        </div>
-                        <br/>
+                        
+                        
                         <center><h2>Nom de la question</h2></center>
                         <br/><br/>
 
                         <center><div class="form-check-inline">
                             <label class="form-check-label">
-                            <input type="radio" class="form-check-input" name="optradio"/>OUI</label>
+                            <input type="radio" class="form-check-input" name="optradio" />OUI</label>
                             </div>
                             <div class="form-check-inline">
                             <label class="form-check-label">
-                            <input type="radio" class="form-check-input" name="optradio"/>NON</label>
+                            <input type="radio" class="form-check-input" name="optradio" />NON</label>
                         </div></center>
                         <br/><br/>
                                
@@ -31,6 +29,11 @@ export default function Question() {
                                 <li class="page-item"><a class="page-link" href="#">Suivante</a></li>
                             </ul>
                         </nav>
+                        <br/>
+                        <center><h5>Complétion du questionnaire</h5></center>
+                        <div class="progress">
+                            <div class="progress-bar bg-success" style={{width:"70%"}}>70%</div>
+                        </div>
                     </div>
                 </div>
 
